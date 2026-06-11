74d7244fd75d44ffc9604307065b4d2606d5bbc9
Peter <ballardpet@gmail.com>
2026-06-11 06:39:35 -0500

    start.bat opens up the web pages now.

--- start.bat
+++ start.bat
@@ -9,6 +9,13 @@ start "gimbal-server" cmd /k "cd /d gimbal-server\src && node server.js"
 echo Starting Dump1090...
 start "Dump1090" cmd /k "cd /d Dump1090 && .\dump1090.exe --interactive --net"
 
+REM Give the servers a few seconds to start
+timeout /t 5 /nobreak >nul
+
+echo Opening browser tabs...
+start "" "http://localhost:5173/"
+start "" "http://localhost:8080/"
+
 echo.
 echo Frontend: http://localhost:5173/
 echo ADS-B Map: http://localhost:8080/
