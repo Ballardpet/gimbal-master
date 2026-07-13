25f4249b70f884b8dd38c5f8727cbb6c5c4dd2f6
Peter <ballardpet@gmail.com>
2026-07-13 16:06:22 -0500

    Can successfully read P5 messages from simulator. Have notes in notebook (7/13/2026) on how to proceed tomorrow.

--- P5 Stuff/Learning UDP/reading_udp.py
+++ P5 Stuff/Learning UDP/reading_udp.py
@@ -1,7 +1,9 @@
 import socket
 
 HOST = "0.0.0.0"  # Listen on all interfaces
-PORT = 5000
+# HOST = "192.168.1.205"  # My address
+
+PORT = 3000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((HOST, PORT))
@@ -25,3 +27,5 @@ finally:
 
 # Next step is to test saving the information it receives. Maybe send something more complex like an object. Then have this program store an array of objects. 
 # Then update on receieve. Or update every second. Something like that.
+
+# Refer to notes in notebook on 7/13/2026 to proceed
