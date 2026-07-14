a8094f3f36a7773cdb4ea3ed65f6f8555b23acc3
Peter <ballardpet@gmail.com>
2026-07-14 08:49:41 -0500

    Successfully parse relevant info from P5 messages, create an aircraft object, and save the object to a dictionary.

--- P5 Stuff/Learning UDP/reading_udp.py
+++ P5 Stuff/Learning UDP/reading_udp.py
@@ -1,20 +1,90 @@
 import socket
+from datetime import datetime
+import struct
+import pymap3d as pm
+
+aircraft_dict = {}
+
+class Aircraft:
+    def __init__(self):
+        self.callsign = ""
+        self.lat = 0.0
+        self.lon = 0.0
+        self.alt = 0.0
+        self.timestamp = None
+
+        # self.site = 0
+        # self.application = 0
+        # self.entity = 0
+
+        # self.force_id = 0
+
+        # self.entity_kind = 0
+        # self.entity_domain = 0
+        # self.entity_country = 0
+        # self.entity_category = 0
+        # self.entity_subcategory = 0
+        # self.entity_specific = 0
+        # slf.entity_extra = 0
+    def print(self):
+        print("Callsign:", self.callsign, "lat:", self.lat, "lon:", self.lon, "alt:", self.alt, "last updated:", self.timestamp)
 
 def parseP5(message):
-    print("Here's what the function recieves:", message)
+    # print("Here's what the function recieves:", message)
     # Isolate callsign
-    callsign = message[129:140]
-    print("callsign", callsign)
+    callsign = message[129:140].rstrip(b"\x00").decode("ascii")
+    # print("callsign", callsign)
 
     # Message contains a callsign, so parse
-    if b'CALLSGN' in callsign:
-        print("This is a P5 message")
+    if 'CALLSGN' in callsign or 'GND' in callsign: # Might need to change this. I Keep getting GND consistently. Maybe this is grounded planes
+        # print("This is a P5 message")
+        # aircraft_id = callsign[len("CALLSGN"):]
+        aircraft_id = callsign # Maybe leave "CALLSGN" in. If we are getting other signs like "GND" for ground or something
+
+        # make a threat type variable. for ground vs air. have ID and callsign/ground separate. Or, just leave it as one big thing
+        
+        # unpack appropriate bytes, convert to a decimal, then un-tuple
+        ecef_x = struct.unpack(">d", message[48:56])[0]
+        ecef_y = struct.unpack(">d", message[56:64])[0]
+        ecef_z = struct.unpack(">d", message[64:72])[0]
+
+        # Convert from ECEF to decimal GPS
+        lat, lon, alt = pm.ecef2geodetic(ecef_x, ecef_y, ecef_z)
+        
+        timestamp = datetime.now() # Maybe periodically delete old aircraft from array        
+        """
+        aircraft = Aircraft()
+        aircraft.callsign = aircraft_id
+        aircraft.lat = lat
+        aircraft.lon = lon
+        aircraft.alt = alt
+        aircraft.timestamp = timestamp
+        """
+
+        if callsign not in aircraft_dict:
+            aircraft = Aircraft()
+            aircraft.callsign = aircraft_id
+            aircraft.lat = lat
+            aircraft.lon = lon
+            aircraft.alt = alt
+            aircraft.timestamp = timestamp
+
+            aircraft_dict[callsign] = aircraft
+        else:
+            aircraft = aircraft_dict[callsign]
+
+            aircraft.lat = lat
+            aircraft.lon = lon
+            aircraft.alt = alt
+            aircraft.timestamp = timestamp
+
+        # print(aircraft_id, lat, lon, alt, timestamp)
+        aircraft.print()
+
         # From here:
             # Extract identification (callsign?)
             # Exctract LLA
-            # Maybe add a timestamp
-                # Last updated at xx:xx:xx
-                # maybe delete if it hasn't been updated in a while
+                # I think LLA is in ECEF, not gps coordinates. Will have to adjust appropriately.
             # Save it to an array or something
                 # Update array if its callsign/id exists
                 # Add as new object if it does exist
@@ -38,8 +108,8 @@ try:
     while True:
         data, addr = sock.recvfrom(4096)  # Buffer size in bytes
         parseP5(data)
-        #print(f"Received {len(data)} bytes from {addr}:")
-        print(data)
+        # print(f"Received {len(data)} bytes from {addr}:")
+        # print(data)
 
 except KeyboardInterrupt:
     print("\nStopping receiver...")
@@ -54,3 +124,40 @@ finally:
 # Then update on receieve. Or update every second. Something like that.
 
 # Refer to notes in notebook on 7/13/2026 to proceed
+
+
+
+# Other info to consider
+"""
+        # Entity Identifier
+        site = struct.unpack(">H", message[12:14])[0]
+        application = struct.unpack(">H", message[14:16])[0]
+        entity = struct.unpack(">H", message[16:18])[0]
+
+        # Force Information
+        force_id = message[18]
+
+        # Entity Type
+        entity_kind = message[20]
+        entity_domain = message[21]
+        entity_country = struct.unpack(">H", message[22:24])[0]
+        entity_category = message[24]
+        entity_subcategory = message[25]
+        entity_specific = message[26]
+        entity_extra = message[27]
+
+
+
+        print(f"Site: {site}")
+        print(f"Application: {application}")
+        print(f"Entity: {entity}")
+        print(f"Force ID: {force_id}")
+
+        print(f"Entity Kind: {entity_kind}")
+        print(f"Entity Domain: {entity_domain}")
+        print(f"Entity Country: {entity_country}")
+        print(f"Entity Category: {entity_category}")
+        print(f"Entity Subcategory: {entity_subcategory}")
+        print(f"Entity Specific: {entity_specific}")
+        print(f"Entity Extra: {entity_extra}")
+        """
